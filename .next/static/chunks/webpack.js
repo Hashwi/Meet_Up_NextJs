3355bf1cb23a093e7c1e7566c77190fba8feb8c5
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-08-10 11:46:10 +0200

    fetching data

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3a3274a9c55860c3"; }
+/******/ 		__webpack_require__.h = function() { return "fdd3a388eabe52cc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,51 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+      {
+        params: {
+          meetupId: "m4",¸
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https//cdn.pixabay.com/photo/2019/05/08/07/45/friends-4187953_1280.png",
+        title: "A First Mettup",
+        address: "02409 Deron Path, 02409 Deron Path, Vermont, 18825-3461",
+        description: "The meetup description",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
